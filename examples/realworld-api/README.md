520149aaa69f01354ddc830f86b2939cb9f64186
iyegoroff <iegoroff@gmail.com>
2022-05-11 04:10:13 +0300

    added example

---
+++ examples/realworld-api/README.md
@@ -0,0 +1,1 @@
+Example app that fetches data from `https://api.realworld.io` and parses received JSON according to [spec](https://github.com/gothinkster/realworld/blob/main/api/openapi.yml).
---
+++ examples/realworld-api/src/index.ts
@@ -0,0 +1,51 @@
+/* eslint-disable node/handle-callback-err */
+import nodeFetch from 'node-fetch'
+import { inspect } from 'util'
+import { Dict } from 'ts-micro-dict'
+import { AsyncResult, Result } from 'ts-railway'
+import { pipe, pipeWith } from 'pipe-ts'
+import { createFetchmap } from 'fetchmap'
+import { number, struct } from 'spectypes'
+
+const fetchmap = createFetchmap(nodeFetch)
+
+const url = (path: string) => `https://api.realworld.io/api/${path}`
+
+const query = (params: Record<string, unknown>) =>
+  new URLSearchParams(Dict.map(String, params)).toString()
+
+const wrapFailure = <S, F, W>(fn: (_: unknown) => Result<S, F>, wrap: (failure: F) => W) =>
+  pipe(fn, Result.mapError(wrap))
+
+const validationFailure = <T>(validationError: T) => ({ validationError } as const)
+
+type ArticlesQueryParams = {
+  readonly tag?: string
+  readonly author?: string
+  readonly favorited?: string
+  readonly limit?: number
+  readonly offset?: number
+}
+
+const checkArticles = struct({ articlesCount: number })
+
+const getArticles = (params: ArticlesQueryParams) =>
+  pipeWith(
+    fetchmap({ ok: 'json' }, url(`articles?${query(params)}`)),
+    AsyncResult.flatMap(wrapFailure(checkArticles, validationFailure))
+  )
+
+const articles = getArticles({})
+
+articles
+  .then(
+    Result.match({
+      success: (arts) => {
+        console.log('getArticles success:', inspect(arts, true, null))
+      },
+      failure: (err) => {
+        console.log('getArticles failure:', inspect(err, true, null))
+      }
+    })
+  )
+  .catch((x) => console.log('should never happen', x))
